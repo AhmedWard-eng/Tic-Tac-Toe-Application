eff7cea0c1b753c386aa6e3e8b6c930383077931
nada hamed <nadahamed723@gmail.com>
2022-12-24 12:44:44 +0200

    finish Fxml_Home UI

--- TicTacToeGame/src/tictactoegame/FXMLDocumentController.java
+++ TicTacToeGame/src/tictactoegame/FXMLDocumentController.java
@@ -21,12 +21,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label label;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- TicTacToeGame/src/tictactoegame/TicTacToeGame.java
+++ TicTacToeGame/src/tictactoegame/TicTacToeGame.java
@@ -19,10 +19,10 @@ public class TicTacToeGame extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = new FXMLHome(stage);
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Tic-Tac-Toe Game");
         stage.setScene(scene);
         stage.show();
     }

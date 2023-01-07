36fdf698321fddc3ecd6e906cf41a3808dc5fc1a
hossamfadaly99 <hossamfdali1@gmail.com>
2023-01-07 14:46:38 +0200

    merging files

--- TicTacToeGame/src/tictactoegame/TicTacToeGame.java
+++ TicTacToeGame/src/tictactoegame/TicTacToeGame.java
@@ -23,7 +23,7 @@ public class TicTacToeGame extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = new FXMLGameHistoryBase(stage);
+        Parent root = new FXMLHomeBase(stage);
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());
 //        System.out.println(getClass().getPackage().);

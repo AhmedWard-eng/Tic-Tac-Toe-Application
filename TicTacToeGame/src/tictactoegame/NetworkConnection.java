0cece74e447f3b2b58d78b50407632dbdbcc1901
marinaAbdelmalak2000 <83098969+marinaAbdelmalak2000@users.noreply.github.com>
2023-01-11 00:02:59 +0200

    recieve string online

--- TicTacToeGame/src/tictactoegame/NetworkConnection.java
+++ TicTacToeGame/src/tictactoegame/NetworkConnection.java
@@ -5,6 +5,8 @@
  */
 package tictactoegame;
 
+import beans.UserOnline;
+import com.google.gson.Gson;
 import com.sun.jndi.dns.DnsContextFactory;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -14,6 +16,7 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 import javafx.application.Platform;
 import javafx.scene.control.TextArea;
@@ -27,11 +30,13 @@ public class NetworkConnection {
     Socket socket;
     BufferedReader bufferedReader;
     PrintStream ps;
-    
+    public static ArrayList<UserOnline> list;
+    public static ArrayList<String> listPlayer;
     
 
     public NetworkConnection() {
         try {
+            
             socket = new Socket(InetAddress.getLocalHost(), 5005);
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             ps = new PrintStream(socket.getOutputStream());
@@ -41,32 +46,57 @@ public class NetworkConnection {
         }
     }
 
+   
     public void readMessage() {
-        RepeatedUserDialog r=new RepeatedUserDialog();
+        RepeatedUserDialog r = new RepeatedUserDialog();
         new Thread() {
             @Override
             public void run() {
                 try {
                     while (socket.isConnected()) {
-                        String str= bufferedReader.readLine();
-                        System.out.println("client recived= "+ str);
-                        if(str.equals("Exist username")){
+                        String str = bufferedReader.readLine();
+                     //   String json_string = str.replaceAll("\r?\n", "");
+                     //   System.out.println("AttaySjson_stringteeeeeeeeeeeeeeesssst" + json_string);
+                        Gson g = new Gson();
+                        UserOnline p = g.fromJson(str, UserOnline.class);
+                        if (p.getStatus().equals("online")) {
+
+                            listPlayer.add(str);
+                            System.out.println("AttayString..." + listPlayer.get(0));
+                            /////////////////////   
+                        //    list.add(p);
+                            for(int i=0;i<list.size();i++)
+//System.out.println("toString array list : "+list.get(0));
+                            /////////////////////
+                            System.out.println("jjjjeeeeeesssssson: " + p.getStatus() );
+//
+//                            for (int i = 0; i < list.size(); i++) {
+//                                System.out.println(list.size() + "SIZE");
+//                            }
+                        } else {
+                            System.out.println("string without jesson....!!!");
+                        }
+
+                        System.out.println("client recived= " + str);
+                        if (str.equals("Exist username")) {
                             Platform.runLater(new Runnable() {
                                 @Override
                                 public void run() {
                                     r.ExistDialog();
                                 }
                             });
-                            
+
                         }
+
                     }
                 } catch (IOException ex) {
                     ex.printStackTrace();
-                    
+
                 }
             }
         }.start();
     }
+
 
     public void sendMessage(String message) {
         new Thread() {
@@ -77,3 +107,4 @@ public class NetworkConnection {
         }.start();
     }
 }
+

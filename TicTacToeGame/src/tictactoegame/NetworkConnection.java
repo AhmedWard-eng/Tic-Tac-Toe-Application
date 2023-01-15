12ecc8614f7b5d670346bf634f72ec40dc3af1ad
Ahmed Ward <ahmedward221@gmail.com>
2023-01-15 09:07:54 +0200

    change the ip

--- TicTacToeGame/src/tictactoegame/NetworkConnection.java
+++ TicTacToeGame/src/tictactoegame/NetworkConnection.java
@@ -69,8 +69,7 @@ public class NetworkConnection {
             //"10.145.19.104"
             if (socket == null || !socket.isConnected() || socket.isClosed()) {
 
-
-                socket = new Socket("192.168.1.4", 5005);
+                socket = new Socket("10.145.19.104", 5005);
 
                 bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 ps = new PrintStream(socket.getOutputStream());
@@ -90,9 +89,7 @@ public class NetworkConnection {
             //"10.145.19.104"
             if (socket == null || !socket.isConnected() || socket.isClosed()) {
 
-
-                socket = new Socket("192.168.1.4", 5005);
-
+                socket = new Socket("10.145.19.104", 5005);
 
                 bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 ps = new PrintStream(socket.getOutputStream());
@@ -115,8 +112,7 @@ public class NetworkConnection {
             //"10.145.19.104"
             if (socket == null || !socket.isConnected() || socket.isClosed()) {
 
-                socket = new Socket("192.168.1.4", 5005);
-
+                socket = new Socket("10.145.19.104", 5005);
 
                 bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 ps = new PrintStream(socket.getOutputStream());
@@ -141,7 +137,7 @@ public class NetworkConnection {
         }
         socket = null;
         if (socket == null || socket.isClosed()) {
-            socket = new Socket("192.168.1.4", 5005);
+            socket = new Socket("10.145.19.104", 5005);
 
             System.out.println("tictactoegame.NetworkConnection.<init>() in constructor testmsg");
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
